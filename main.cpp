031981a5def81c9cac7030340587b45f6bf4edad
Kirhhoff <1070280566@qq.com>
2018-06-28 15:36:08 +0800

    Time test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<ctime>
 
 int main(){
+	time_t ta=clock();
 	Sequence tmp("dna.txt");
 	cout<<"A: "<<tmp.numberOf('A')<<endl;
 	cout<<"C: "<<tmp.numberOf('C')<<endl;
@@ -9,6 +10,8 @@ int main(){
 	cout<<"G: "<<tmp.numberOf('G')<<endl;
 	cout<<"Sum: "<<tmp.length()<<endl;
 	cout<<tmp.longestConsecutive()<<endl;
+	time_t tb=clock();
+	cout<<"Time: "<<(double)(tb-ta)/CLOCKS_PER_SEC<<endl;
 	time_t t1=clock();
 	cout<<tmp.longestRepeated()<<endl;
 	time_t t2=clock();
